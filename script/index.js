e5afcef06979d4e92afd5f9901624ded499953f7
Anton Zabrodsky <antontti@yandex.ru>
2022-06-03 20:11:24 +0300

    fixed: after 1 review

--- script/index.js
+++ script/index.js
@@ -1,23 +1,25 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
-
-editButton.addEventListener('click', function () {
-    popup.classList.remove('popup_hidden');
-});
-
-popupCloseButton.addEventListener('click', function () {
-    popup.classList.add('popup_hidden');
-});
-
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let formElement = document.querySelector('.form')
 let nameInput = document.querySelector('.form__input_type_name')
 let aboutInput = document.querySelector('.form__input_type_about')
 
-nameInput.value = profileName.textContent;
-aboutInput.value = profileDescription.textContent;
+function openPopup() {
+    popup.classList.remove('popup_hidden');
+};
+
+function closePopup() {
+    popup.classList.add('popup_hidden');
+};
+
+function fillForm() {
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileDescription.textContent;
+};
+
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -26,4 +28,9 @@ function formSubmitHandler(evt) {
     popup.classList.add('popup_hidden');
 }
 
+editButton.addEventListener('click', function () {
+    openPopup()
+    fillForm()
+});
+popupCloseButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
